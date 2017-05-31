c92deeab50a900f63a22f6a49f5f07368805b176
Palonen <ppalonen@bethel.jw.org>
2017-05-31 16:13:20 +0200

    Archived from Codebender

---
+++ lcd.h
@@ -0,0 +1,152 @@
+PROGMEM  prog_uint8_t batDead_char[]  = { 
+  B01110,
+  B11011,
+  B10001,
+  B10000,
+  B00000,
+  B00001,
+  B10001,
+  B11111,
+};
+PROGMEM  prog_uint8_t solar_char[]  = { 
+  B11111,
+  B10101,
+  B11111,
+  B10101,
+  B11111,
+  B10101,
+  B11111,
+  B00000,
+};
+PROGMEM  prog_uint8_t temperature_char[] = {
+  B00100,
+  B01010,
+  B01010,
+  B01110,
+  B01110,
+  B11111,
+  B11111,
+  B01110,
+};
+PROGMEM  prog_uint8_t celsium_char[] = {
+  B01000,
+  B10100,
+  B01000,
+  B00000,
+  B00111,
+  B01000,
+  B01000,
+  B00111,
+};
+PROGMEM  prog_uint8_t watthour_char[] = {
+  B10001,
+  B10101,
+  B01010,
+  B00000,
+  B10000,
+  B11100,
+  B10010,
+  B10010,
+};
+
+PROGMEM  prog_uint8_t batEmpty_char[] = {
+  B01110,
+  B11011,
+  B10001,
+  B10001,
+  B10001,
+  B10001,
+  B10001,
+  B11111,
+};
+PROGMEM  prog_uint8_t bat10_char[] = {
+  B01110,
+  B11011,
+  B10001,
+  B10001,
+  B10001,
+  B10001,
+  B11111,
+  B11111,
+};
+PROGMEM  prog_uint8_t bat30_char[] = {
+  B01110,
+  B11011,
+  B10001,
+  B10001,
+  B10001,
+  B11111,
+  B11111,
+  B11111,
+};
+PROGMEM  prog_uint8_t bat50_char[] = {
+  B01110,
+  B11011,
+  B10001,
+  B10001,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+};
+PROGMEM  prog_uint8_t bat70_char[] = {
+  B01110,
+  B11011,
+  B10001,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+};
+PROGMEM  prog_uint8_t bat90_char[] = {
+  B01110,
+  B11011,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+};
+PROGMEM  prog_uint8_t batFull_char[] = {
+  B01110,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+};
+PROGMEM  prog_uint8_t ampers_char[] = {
+  B00010,
+  B00100,
+  B01000,
+  B11111,
+  B00010,
+  B00100,
+  B01000,
+  B00000,
+};
+
+PROGMEM  prog_uint8_t loadOff_char[] = {
+  B10000,
+  B10000,
+  B11000,
+  B00100,
+  B00010,
+  B11001,
+  B10000,
+  B10000,
+};
+PROGMEM  prog_uint8_t loadOn_char[] = {
+  B10000,
+  B10000,
+  B11000,
+  B01000,
+  B01000,
+  B11000,
+  B10000,
+  B10000,
+};
